670a0da29a71f4f6ad4e2f8d80dcc5966c44bafc
Monika Yadav <monikayadav_100@yahoo.com>
2023-07-23 22:12:57 +0530

    Update WeatherDataTest.java

--- src/main/java/com/visualcrossing/test/WeatherDataTest.java
+++ src/main/java/com/visualcrossing/test/WeatherDataTest.java
@@ -10,28 +10,7 @@ public class WeatherDataTest  extends TestBase {
 //	HomePage homePage = new HomePage(driver);
 //	WeatherDatePage weatherdataPage = new WeatherDatePage(driver);
 
-	
-//	@Test
-//    public void testSearchLocation() {
-//
-//        // Initialize the MainPage object
-//
-//        // Interact with the elements using the Page Object methods
-//		homePage.clickWeatherDataMenu();
-//
-//        // Step 3: Enter your current city in the text field and press "Search" button
-//        String currentCity = "New York"; // Replace with your current city
-////        homePage.enterLocation(currentCity);
-////        homePage.clickSearchButton();
-////        Assert.assertTrue(homePage.isWeatherForecastVisible(), "Weather forecast for " + currentCity + " is not shown.");
-//
-//       
-//        // Add additional test steps and assertions as required based on the website functionality
-//        // For example, you can use TestNG's assertions to verify certain conditions.
-//        // Example: Assert.assertTrue(driver.getTitle().contains("Expected Title"));
-//        // Add more test scenarios and assertions as required based on the requirements.
-//        // For example, you can verify specific weather data like temperature, humidity, etc.
-//    }
+
 
 //	@Test
 //	public void verifyTitle() {
